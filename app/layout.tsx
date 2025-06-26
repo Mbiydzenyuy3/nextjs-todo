674fea51970de3a047ee47bec3c632238055ee37
leila237 <mbiydzenyuyeileen@gmail.com>
2025-06-26 15:01:31 +0100

    added new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ export default function Todolist({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        {/* Layout UI */}
-        {/* Place children where you want to render a page or nested layout */}
         <main>{children}</main>
       </body>
     </html>
