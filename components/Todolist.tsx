226cfee25e32ab1fa10586f181da296cbb8f6868
leila237 <mbiydzenyuyeileen@gmail.com>
2025-07-06 17:44:42 +0100

    added new commit

--- components/Todolist.tsx
+++ components/Todolist.tsx
@@ -1,17 +1,18 @@
 'use strict'
-
 interface Todolist {
   name: string
   date: number
   isDue: boolean
 }
 
-// const todolist:Todolist  = { name:"alice", date: 25, isDue: false}
+const todolist: Todolist = { name: 'alice', date: 25, isDue: false }
 
 export default function Todolist() {
   return (
     <>
-      <div className="todo"></div>
+      <div className="todo">
+        <h1>ToDo List</h1>
+      </div>
     </>
   )
 }
