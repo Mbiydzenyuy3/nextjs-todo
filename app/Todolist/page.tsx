65e52b041a3eb67a94abbc3048b25ad8c7ccb447
leila237 <mbiydzenyuyeileen@gmail.com>
2025-07-07 20:00:43 +0100

    made changes

--- app/Todolist/page.tsx
+++ app/Todolist/page.tsx
@@ -1,8 +1,16 @@
 'use strict'
+import Button from '@/components/button'
+import MyTodolist from '@/components/MyTodolist'
 
 export default function Todolist() {
-  
-  return <>
-     <div className="todo"></div>
-  </>
+  return (
+    <>
+      <div className="todo">
+        <div className="todo">
+          <MyTodolist />
+        </div>
+        <Button />
+      </div>
+    </>
+  )
 }
--- components/MyTodolist.tsx
+++ components/MyTodolist.tsx
@@ -1,13 +1,13 @@
 'use strict'
-interface Todolist {
+interface MyTodolist {
   name: string
   date: number
   isDue: boolean
 }
 
-const todolist: Todolist = { name: 'alice', date: 25, isDue: false }
+const todolist: MyTodolist = { name: 'alice', date: 25, isDue: false }
 
-export default function Todolist() {
+export default function MyTodolist() {
   return (
     <>
       <div className="todo">
